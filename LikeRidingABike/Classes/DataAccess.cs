37cd0289278ea2b111daf7149d62ecf55722b235
tomre <tom.redleo@gmail.com>
2023-09-13 11:10:56 +1000

    Comitting to continue work on other PC

---
+++ LikeRidingABike/Classes/DataAccess.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace LikeRidingABike.Classes
+{
+    public class DataAccess
+    {
+        private string connectionString;
+
+        public DataAccess(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void OpenConnection()
+        {
+            // Open the SQLite connection here
+        }
+
+        public void CloseConnection()
+        {
+            // Close the SQLite connection here
+        }
+
+        // Other methods for executing SQL commands and managing data
+    }
+}
